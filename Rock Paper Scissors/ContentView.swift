57b08bdd552fca6780806788a808df8a0e96c7a2
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-05-24 14:31:12 +0100

    Initial Logic

--- Rock Paper Scissors/ContentView.swift
+++ Rock Paper Scissors/ContentView.swift
@@ -8,7 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let listOfChoices = ["Rock", "Paper" , "Scissors"]
+    let winOrLose = ["Win", "Lose"].shuffled()
+    
+    @State private var computerChoice = Int.random(in: 0...2)
+    @State private var winLose = Int.random(in: 0..<2)
+    @State private var gameCount = 0
+    @State private var usersChoice = "Rock"
+    
     var body: some View {
+        Text("Computers choice is: \(listOfChoices[computerChoice])")
+        Text("Your aim to \(winOrLose[winLose])")
+        HStack(spacing: 30){
+            Button("Rock"){
+                print("User Chose Rock")
+            }
+            Button("Paper"){
+                print("User Chose Paper")
+            }
+            Button("Scissors"){
+                print("User Chose Scissors")
+            }
+        }
     }
 }
 

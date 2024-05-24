b04cf1a6e5aa47b300d7e40112bd78fd318f9f3f
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-05-24 13:53:23 +0100

    Initial Commit

---
+++ Rock Paper Scissors/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissors
+//
+//  Created by Mehmet Alp Sönmez on 24/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Mehmet Alp Sönmez on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
